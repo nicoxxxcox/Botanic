0e096488aa389d4be03f2d919bb69dc348119600
NicolasG <nicolas.grissi@gmail.com>
2022-01-16 14:33:18 +0100

    Revert "build app at root to serve on gh-pages"
    This reverts commit 622ce8efdb4d24ba2257513280839776117fa79f.

--- index.a2fde7cc.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".burger-icon"),c=document.querySelector(".nav-wrapper");c.classList.remove("is-open"),e.addEventListener("click",(function(){c.classList.toggle("is-open")}));
-//# sourceMappingURL=index.a2fde7cc.js.map
